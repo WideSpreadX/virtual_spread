674f38e600bd3051ec234bded0c245d413e54975
Jonathan Olson <48499429+WideSpreadX@users.noreply.github.com>
2020-03-08 17:11:48 -0400

    Switch back to a-link & title=

--- ar-spread.js
+++ ar-spread.js
@@ -42,12 +42,12 @@ window.onload = () => {
           const longitude = place.location.lng;
 
           // add place name
-          const placeText = document.createElement("a-text");
+          const placeText = document.createElement("a-link");
           placeText.setAttribute(
             "gps-entity-place",
             `latitude: ${latitude}; longitude: ${longitude};`
           );
-          placeText.setAttribute("value", place.name);
+          placeText.setAttribute("title", place.name);
           placeText.setAttribute("scale", "15 15 15");
 
           placeText.addEventListener("loaded", () => {
